aee84016ea87dd0d401c76edf041983baa314f38
suryapendimi <surya_pendimi@yahoo.com>
2022-06-25 00:16:58 +0530

    InitialCommit
    InitialCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeDetailComponent,
+    RecipesComponent,
+    RecipeItemComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
